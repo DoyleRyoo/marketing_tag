c52bd5e4aa2cd194cfe1fbeecd9c488e05fd6599
doyleryoo <ryodoil1812@gmail.com>
2026-07-13 11:16:49 +0900

    change to HTML/CSS UI with Electron

---
+++ src/main/main.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from "electron/main";
+import path from "node:path";
+
+function createWindow(): void {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      preload: path.join(__dirname, "../preload/preload.js"),
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, "../renderer/index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
